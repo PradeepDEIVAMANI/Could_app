6dd09f918bc1f1e71700ab5342bfb4b7a1920208
Pradeep DEIVAMANI <133005962+PradeepDEIVAMANI@users.noreply.github.com>
2024-01-30 14:28:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import images from './images'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Apple Store</h1>
+      <div className="image-container">
+        <img src="/1.jpg" alt="Image 1" />
+        <img src="/2.jpg" alt="Image 2" />
+        <img src="/3.jpg" alt="Image 3" />
+        <img src="/4.jpg" alt="Image 4" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
